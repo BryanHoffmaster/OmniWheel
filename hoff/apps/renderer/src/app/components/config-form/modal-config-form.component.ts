788a3dd7276593a000c5f21b5168a9ab0fe3a690
Bryan Hoffmaster <bryan.hoffmaster@gmail.com>
2024-05-28 10:06:55 -0500

    Setup basic form modal form creation and close features

--- hoff/apps/renderer/src/app/components/config-form/modal-config-form.component.ts
+++ hoff/apps/renderer/src/app/components/config-form/modal-config-form.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, ViewContainerRef, inject } from '@angular/core';
+import { ConfigService } from '../../services/config.service';
 
 @Component({
   selector: 'app-modal-config-form',
@@ -9,4 +10,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
   styleUrl: './modal-config-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ModalConfigFormComponent {}
+export class ModalConfigFormComponent {
+  private vcr = inject(ViewContainerRef)
+  private configService = inject(ConfigService)
+
+  // eslint-disable-next-line @angular-eslint/no-output-native
+  @Output() close: EventEmitter<void> = new EventEmitter<void>()
+
+  // setup a simple close emitter for the parent to destroy
+  onClose() {
+    this.close.emit()
+  }
+
+  // Need an import type function to pull in the current config and loop over all
+  // the options and have them build the form dynamically.
+}
--- hoff/apps/renderer/src/app/components/omniwheel/parts/hub/hub.component.ts
+++ hoff/apps/renderer/src/app/components/omniwheel/parts/hub/hub.component.ts
@@ -1,27 +1,55 @@
 import { CommonModule } from '@angular/common'
-import { AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, ViewChild, ViewContainerRef, inject, input } from '@angular/core'
+import { AfterViewInit, ChangeDetectionStrategy, Component, ComponentRef, HostListener, OnDestroy, ViewChild, ViewContainerRef, inject, input } from '@angular/core'
 import { ConfigService } from 'apps/renderer/src/app/services/config.service'
 import { Subscription } from 'rxjs'
 import { Node, Omniwheel } from 'shared'
+import { ModalConfigFormComponent } from '../../../config-form/modal-config-form.component'
 import { NodeComponent } from '../node/node.component'
+
+// TODO: set title for host element to something like "Open Omniwheel Configuration"
 
 @Component({
   selector: 'app-hub',
   standalone: true,
-  imports: [CommonModule, NodeComponent],
+  imports: [CommonModule, NodeComponent, ModalConfigFormComponent],
   templateUrl: './hub.component.html',
   styleUrl: './hub.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HubComponent implements OnDestroy, AfterViewInit {
   private configService = inject(ConfigService)
-  private nodes: NodeComponent[] = []
   private subs: Subscription[] = []
+  private nodes: NodeComponent[] = []
+  private vcr = inject(ViewContainerRef);
+  private modalConfigRef: ComponentRef<ModalConfigFormComponent> | null = null;
 
   @ViewChild('wheelNodeContainer', { read: ViewContainerRef })
   private wheelNodeContainer!: ViewContainerRef
 
   config = input.required<Omniwheel>()
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    // prevent bubbling
+    event.stopImmediatePropagation()
+    event.stopPropagation()
+
+    if (this.modalConfigRef) {
+      // prevent multiple modals from being open at once
+      return;
+    }
+
+    this.modalConfigRef = this.vcr.createComponent(ModalConfigFormComponent)
+    this.modalConfigRef.changeDetectorRef.detectChanges()
+    this.modalConfigRef.instance.close.subscribe(() => {
+      this.modalConfigRef?.destroy()
+    })
+    this.modalConfigRef.onDestroy(() => {
+      this.modalConfigRef = null
+    })
+  }
+
+
 
   ngAfterViewInit() {
     this.buildNodes(this.config().nodes)
